ed47e4d47c0f5daa0960dfd3b2f92971fe3da44f
salih <muhammetsalihaslan@gmail.com>
2023-01-04 14:03:13 +0300

    set up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import Form from "./Form";
 
 const Header = () => {
   return (
     <header className="header">
       <h1>todos</h1>
-      <form>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autofocus
-        />
-      </form>
+      <Form />
     </header>
   );
 };
